b7826cb06ad39f8911095a7dd724f4672b92ed8b
Jonathan Babel <jonathan.babel@dfki.de>
2021-05-28 17:44:57 +0200

    starting a setup file

---
+++ sw/python/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Torque Limited Simple Pendulum',
+    author= 'Underactuated LAB at DFKI Bremen',
+    version='0.1.0',
+    #url=
+    packages=find_packages(),
+    install_requires=[
+        #general
+        'numpy',
+        'pyyaml',
+        'pandas',
+        'argparse',
+
+        # reinforcement learning
+        'stable_baselines'
+    ]
+    classifiers=[
+          'Development Status :: 0.1.0 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Academic Usage,
+          'Programming Language :: Python',
+          ],
+)
