e304548dda9f3462c2be11b7a9f386fed0a8dfc9
Melya Boukheddimi <mboukheddimi@VHUMAN-P15-U.dfki.uni-bremen.de>
2021-05-31 15:25:17 +0200

    problem for the simple pundulun using BOXFDDP with crocoddyl

--- sw/python/controllers/OC_DDP_SimplePendulum/problem_simple_BOXFDDP_offfline.py
+++ sw/python/controllers/OC_DDP_SimplePendulum/problem_simple_BOXFDDP_offfline.py
@@ -11,27 +11,24 @@ import os
 import sys
 import math
 import matplotlib.pyplot as plt
-
+#crocoddyl imports
 import crocoddyl
-
-import example_robot_data
 from crocoddyl.utils.pendulum import CostModelDoublePendulum, ActuationModelDoublePendulum
-
+#pinocchio imports
 from pinocchio.robot_wrapper import RobotWrapper
-
 import pinocchio
 from pinocchio.utils import *
 from pinocchio import GeometryModel, GeometryObject
 import pinocchio as pin
-
+#import the robots with data
+import example_robot_data
+# import the driver file in order to control the pundulum
 from canmotorlib import CanMotorController
 
 
-
-
-
-
 #********************************************************************************************************************************
+
+# for controlling the robot using the driver 
 '''
 
 # Motor ID
@@ -69,42 +66,47 @@ time.sleep(1)
 #********************************************************************************************************************************
 
 # Loading the double pendulum model
-filename="./simplependul_dfki_pino.urdf"
+filename="~/simplependul_dfki_pino.urdf"
 
-
+#building the robot from the urdf
 robot =  RobotWrapper.BuildFromURDF(filename)
 
-#robot =  RobotWrapper.BuildFromURDF(filename, [])
-
-#robot = example_robot_data.load('double_pendulum')
-
-
+#creating the robot model and data in Crocoddyl
 robot_model = robot.model
 rmodel = robot.model
 rdata  = robot_model.createData()
 
+#adding the toques and velociies limites 
 lims = rmodel.effortLimit
 
 lims = rmodel.velocityLimit
 
 #********************************************************************************************************************************
-
+# definning the state ad actuation model
 state = crocoddyl.StateMultibody(robot_model)
-actModel = ActuationModelDoublePendulum(state, actLink=1) # the catuated link is the 2
+actModel = ActuationModelDoublePendulum(state, actLink=1) # the actuated link is the 1, in the case of a double pundulum you have to change it
 
 
 #********************************************************************************************************************************
+# definning the final desired state =[q, dot(q)]
 stateF = np.asarray([3.14159, 0.])
 
 #********************************************************************************************************************************
 
-weights = np.array([1, 1] + [0.1] * 1)
+# 
+#weights = np.array([1] + [0.1] * 1)  # not used in this script but could be used to tune the "crocoddyl.ActivationModelQuad" in the state and ontrol costs by remplacing (state.ndx) by your weights
 
+#********************************************************************************************************************************
+
+# defining the running cost model as a sun of cost
 runningCostModel = crocoddyl.CostModelSum(state, actModel.nu)
+
+# difining the terminal cost model as a sun of cost
 terminalCostModel = crocoddyl.CostModelSum(state, actModel.nu)
 
 #********************************************************************************************************************************
 
+# definning the costs for the running and the terminal model
 xRegCost = crocoddyl.CostModelState(state, crocoddyl.ActivationModelQuad(state.ndx), stateF, actModel.nu)
 uRegCost = crocoddyl.CostModelControl(state, crocoddyl.ActivationModelQuad(1), actModel.nu)
 
@@ -112,19 +114,30 @@ xPendCost = crocoddyl.CostModelState(state, crocoddyl.ActivationModelQuad(state.
 
 #********************************************************************************************************************************
 
+#definning the dt, the discretization step, the time between each node
 dt = 1e-3
+
+#definning the number of nodes (time horizon)
+T = 4000
+
+#********************************************************************************************************************************
 
 # Extract Time
 time_traj = np.linspace(0.0000, 4.0000, 4000)
 time_traj = time_traj.reshape(4000, 1).T
 print("Time Array Shape: {}".format(time_traj.shape))
 
+#********************************************************************************************************************************
 
-#adding costs
+
+#adding costs for the running model
 runningCostModel.addCost("xGoal", xPendCost, 0.15)
 runningCostModel.addCost("uReg", uRegCost, 0.000010)
 
+#adding costs for the terminal model
 terminalCostModel.addCost("xGoal", xPendCost, 1e10)
+
+#********************************************************************************************************************************
 
 #models
 runningModel = crocoddyl.IntegratedActionModelEuler(
@@ -133,29 +146,35 @@ runningModel = crocoddyl.IntegratedActionModelEuler(
 terminalModel = crocoddyl.IntegratedActionModelEuler(
     crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actModel, terminalCostModel), dt)
 
-# Creating the shooting problem and the FDDP solver
+#********************************************************************************************************************************
 
-
-T = 4000
+#definning the initial state
 x0 = np.array([0., 0.])
 
+# Creating the shooting problem and the FDDP solver
 
 problem = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
 fddp = crocoddyl.SolverBoxFDDP(problem)
 
 
+#********************************************************************************************************************************
 
-
-
+#for the display definning the position and orientation of the camera by a quaternion
 cameraTF = [1.4, 0., 0.2, 0.5, 0.5, 0.5, 0.5]
+
+#********************************************************************************************************************************
 
 display = crocoddyl.GepettoDisplay(robot, 4, 4, cameraTF, True)
 fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(display)])
-
+#********************************************************************************************************************************
+#to test computational time
 startdtTest = time.time()
+#********************************************************************************************************************************
 
 # Solving the problem with the FDDP solver
 fddp.solve()
+
+#********************************************************************************************************************************
 
 enddtTest = time.time()
 
@@ -164,10 +183,13 @@ cmd_freq = 1 / dt
 print("Dt = {}".format(dt))
 print("Command Frequency: {} Hz".format(cmd_freq))
 
+#********************************************************************************************************************************
+
 log = fddp.getCallbacks()[0]
 
 
 #*********************************************************************************************************************************************************8
+#saving the data as a nuöpy files and as .csv in order to send them to the robot
 
 torque = []
 Q = []
@@ -191,10 +213,6 @@ theta = ROT[:].reshape(T,1).T
 VEL = np.asarray(V)
 theta_dot = VEL[:].reshape(T,1).T
 
-'''
-kp = 50
-kd = 2*np.sqrt(kp)
-'''
 
 np.save('torque.npy',torque)
 np.save('rotations.npy',Q)
