b74625b1e9e84e22bc926a44f86605cc68584364
jbabel <jonathan.babel@dfki.de>
2021-06-01 22:11:31 +0200

    first version of the wiring diagram

--- sw/python/controllers/__init__.py
+++ sw/python/controllers/__init__.py
@@ -1,4 +1,4 @@
 
-__all__ = ["control_loop", "pd", "gravity_compensation", "lqr", "ddp" ]
+__all__ = ["control_loop", "pd", "gravity_compensation", "lqr", "ilqr", "energy_shaping", "mpc_direct_collocation", "open_loop", "graph_search", "ddp", "rl", ]
 
 
