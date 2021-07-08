9b653d4ed12d23bee69b80e6c29ca5c579cf12d3
Jonathan Babel <jonathan.babel@dfki.de>
2021-07-08 15:54:53 +0200

    conclude merge
    Former-commit-id: 59798df72b4359cf50f3e53cd0340d14d49573d3

--- sw/python/controllers/sac/sac_controller.py
+++ sw/python/controllers/sac/sac_controller.py
@@ -14,27 +14,28 @@ from controllers.abstract_controller import AbstractClosedLoopController
 
 # default parameters, can be changed
 model_path = os.path.join(Path(__file__).parents[4], 'data/models/sac_model.zip')
-params_path = os.path.join(Path(__file__).parents[4],
-                           'data/parameters/sp_parameters_sac.yaml')
+# params_path = os.path.join(Path(__file__).parents[4],
+#                            'data/parameters/sp_parameters_sac.yaml')
 
 
 class SacController(AbstractClosedLoopController):
-    def __init__(self, model_path=model_path):
+    def __init__(self, params):
 
         self.model = SAC.load(model_path)
-        self.params = None
-
-    def get_params(self, params_path=params_path):
-        with open(params_path, 'r') as fle:
-            params = yaml.safe_load(fle)
-            self.params = params
-        return params
+        self.params = params
 
     def get_control_output(self, meas_pos, meas_vel, meas_tau, meas_time=0):
-
         observation = np.squeeze(np.array([meas_pos, meas_vel]))
-        des_tau, _states = self.model.predict(observation)
+        if self.params['use_symmetry']:
+            observation[0] *= np.sign(meas_pos)
+            observation[1] *= np.sign(meas_pos)
+            des_tau, _states = self.model.predict(observation)
+            des_tau *= np.sign(meas_pos)
+        else:
+            des_tau, _states = self.model.predict(observation)
         des_tau *= float(self.params['torque_limit'])
+
+
         
         # since this is a pure torque controller, set pos_des and vel_des to None
         des_pos = None
