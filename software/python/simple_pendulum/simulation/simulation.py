3d0f6786f110b59e1d007682d1a9f38663787a32
felix.wiebe <felix.wiebe@dfki.de>
2021-10-01 10:53:28 +0200

    added an arrow indicating the control torque to the simulation animation
    Former-commit-id: 374ca41898084b18e76ae9dcc6de8be87b186d75

--- software/python/simple_pendulum/simulation/simulation.py
+++ software/python/simple_pendulum/simulation/simulation.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as mplanimation
+from matplotlib.patches import Arc, RegularPolygon
+from numpy import radians as rad
 
 
 class Simulator:
@@ -164,7 +166,8 @@ class Simulator:
         tf: float
             final time, unit: s
         controller: A controller object of the type of the
-                    AbstractController in simple_pendulum.controllers.abstract_controller.py
+                    AbstractController in
+                    simple_pendulum.controllers.abstract_controller.py
                     If None, a free pendulum is simulated.
         integrator: string
             "euler" for euler integrator,
@@ -210,7 +213,22 @@ class Simulator:
         for ap in self.animation_plots[:-1]:
             ap.set_data([], [])
         self.animation_plots[-1].set_text("t = 0.000")
-        return self.animation_plots
+
+        self.tau_arrowarcs = []
+        self.tau_arrowheads = []
+        for link in range(self.plant.n_links):
+            arc, head = get_arrow(radius=0.001,
+                                  centX=0,
+                                  centY=0,
+                                  angle_=110,
+                                  theta2_=320,
+                                  color_="red")
+            self.tau_arrowarcs.append(arc)
+            self.tau_arrowheads.append(head)
+            self.animation_ax.add_patch(arc)
+            self.animation_ax.add_patch(head)
+
+        return self.animation_plots + self.tau_arrowarcs + self.tau_arrowheads
 
     def _animation_step(self, par_dict):
         """
@@ -241,6 +259,12 @@ class Simulator:
                             [ee_pos[link][0], ee_pos[link+1][0]],
                             [ee_pos[link][1], ee_pos[link+1][1]])
             ani_plot_counter += 1
+
+            set_arrow_properties(self.tau_arrowarcs[link],
+                                 self.tau_arrowheads[link],
+                                 float(np.squeeze(tau)),
+                                 ee_pos[link][0],
+                                 ee_pos[link][1])
         t = float(self.animation_plots[ani_plot_counter].get_text()[4:])
         t = round(t+dt, 3)
         self.animation_plots[ani_plot_counter].set_text(f"t = {t}")
@@ -251,7 +275,7 @@ class Simulator:
             self.animation_plots[ani_plot_counter].set_color("red")
         else:
             self.animation_plots[ani_plot_counter].set_color("black")
-        return self.animation_plots
+        return self.animation_plots + self.tau_arrowarcs + self.tau_arrowheads
 
     def _ps_init(self):
         """
@@ -373,3 +397,58 @@ class Simulator:
         plt.show()
 
         return self.t_values, self.x_values, self.tau_values
+
+
+def get_arrow(radius, centX, centY, angle_, theta2_, color_='black'):
+    arc = Arc([centX, centY],
+              radius,
+              radius,
+              angle=angle_,
+              theta1=0,
+              theta2=theta2_,
+              capstyle='round',
+              linestyle='-',
+              lw=2,
+              color=color_)
+
+    endX = centX+(radius/2)*np.cos(rad(theta2_+angle_))
+    endY = centY+(radius/2)*np.sin(rad(theta2_+angle_))
+
+    head = RegularPolygon((endX, endY),            # (x,y)
+                          3,                       # number of vertices
+                          radius/20,               # radius
+                          rad(angle_+theta2_),     # orientation
+                          color=color_)
+    return arc, head
+
+
+def set_arrow_properties(arc, head, tau, x, y):
+    tau_rad = np.clip(0.1*np.abs(tau) + 0.1, -1, 1)
+    if tau > 0:
+        theta2 = -40
+        arrow_angle = 110
+        endX = x+(tau_rad/2)*np.cos(rad(theta2+arrow_angle))
+        endY = y+(tau_rad/2)*np.sin(rad(theta2+arrow_angle))
+        orientation = rad(arrow_angle + theta2)
+    else:
+        theta2 = 320
+        arrow_angle = 110
+        endX = x+(tau_rad/2)*np.cos(rad(arrow_angle))
+        endY = y+(tau_rad/2)*np.sin(rad(arrow_angle))
+        orientation = rad(-arrow_angle-theta2)
+    arc.center = [x, y]
+    arc.width = tau_rad
+    arc.height = tau_rad
+    arc.angle = arrow_angle
+    arc.theta2 = theta2
+
+    head.xy = [endX, endY]
+    head.radius = tau_rad/20
+    head.orientation = orientation
+
+    if np.abs(tau) <= 0.01:
+        arc.set_visible(False)
+        head.set_visible(False)
+    else:
+        arc.set_visible(True)
+        head.set_visible(True)
