fa03b5b7a9364cd1b86c33dd382a673a1165441b
Jonathan Babel <jonathan.babel@dfki.de>
2021-10-13 09:54:38 +0000

    Update parse.py
    Former-commit-id: 857bf346e9ada0a9288ee74ce72c9778c90a3586

--- software/python/simple_pendulum/utilities/parse.py
+++ software/python/simple_pendulum/utilities/parse.py
@@ -8,6 +8,23 @@ def syntax():
         description='''                     Control of a torque limited Simple Pendulum
 
                             Underactuated LAB at DFKI Bremen
+
+
+        Besides the distinction in model-based controllers (gravity compensation, energy shaping, fddp, lqr, ilqr) 
+        and data-driven controllers (sac, ddpg) the control methods are also divided into open and closed loop control. 
+        The former execute a precomputed trajectory, which is derived from one out of two trajectory optimization 
+        techniques:
+
+            - Direct Collocation
+            - Feasibility-Driven Dynamic Programming
+
+        The latter don't rely on a precomputed trajectory, but generate their input torques online based on the 
+        implemented control policy. You can choose from a variety of different control methods by adding the flag 
+        of your desired controller, e.g. '-lqr' or '-energy', to 'main.py'. In order to use openloop controllers 
+        you additionally need to specify the '-openloop' flag, for example to run the real pendulum with 
+        Feasibility-Driven Dynamic Programming the corresponding terminal command would be 'python main.py -openloop -fddp'. 
+        We further provide the option to save all measurements and plots in the results folder by adding the -save flag.
+
         ''', formatter_class=RawTextHelpFormatter)
     controller = parser.add_mutually_exclusive_group(required=True)
     controller.add_argument("-gravity", action='store_true',
@@ -22,14 +39,14 @@ def syntax():
                             help="soft actor critic")
     controller.add_argument("-openloop", action='store_true',
                             help="open loop control mode, choose either -pd "
-                                 "or -ftt ")
+                                 "or -fft ")
     openloop = parser.add_mutually_exclusive_group(required=False)
     openloop.add_argument("-pd", action='store_true',
                           help="proportional derivative control mode")
     openloop.add_argument("-fft", action='store_true',
                           help="feedforward torque control mode")
-    openloop.add_argument("-ddp", action='store_true',
-                          help="differential dynamic programming")
+    openloop.add_argument("-fddp", action='store_true',
+                          help="feasability-driven differential dynamic programming")
     parser.add_argument("-save", action='store_true',
                         help="saves your measurements into (../results)",
                         required=False)
